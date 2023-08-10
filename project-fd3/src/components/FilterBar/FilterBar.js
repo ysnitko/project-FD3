4363861ad19c92936f61af4e23225c9953d8a77f
yuri_sn <yuri.snitko@gmail.com>
2023-08-11 01:23:52 +0300

    add sort

--- project-fd3/src/components/FilterBar/FilterBar.js
+++ project-fd3/src/components/FilterBar/FilterBar.js
@@ -1,127 +1,59 @@
-import React from "react";
-import "./FilterBar.css";
+import React, { useState } from 'react';
+import './FilterBar.css';
 
 const FilterBar = ({ movieList, setStartList, setMovieList }) => {
-  console.log("render FilterBar");
+  console.log('render FilterBar');
+
+  // const initialList = useMemo(() => movieList, [movieList]);
+  const [filteredMovieList, setFilteredMovieList] = useState([]);
+  // const [sortedMovieList, setSortedMovieList] = useState([]);
+
   const handleSort = (event) => {
-    const sortedList = [...movieList];
-    if (event.target.value === "by-title") {
+    const sortedList = [...filteredMovieList];
+    if (event.target.value === 'by-title') {
       sortedList.sort((a, b) => {
         return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
       });
     }
 
-    if (event.target.value === "by-title-reverse") {
+    if (event.target.value === 'by-title-reverse') {
       sortedList.sort((a, b) => {
         return a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1;
       });
     }
 
-    if (event.target.value === "by-rating-start") {
+    if (event.target.value === 'by-rating-start') {
       sortedList.sort((a, b) => {
         return a.rating.average > b.rating.average ? 1 : -1;
       });
     }
 
-    if (event.target.value === "by-rating-end") {
+    if (event.target.value === 'by-rating-end') {
       sortedList.sort((a, b) => {
         return a.rating.average > b.rating.average ? -1 : 1;
       });
     }
 
     setMovieList(sortedList);
-    setStartList(sortedList.slice(0, 15));
+    // setMovieList(sortedList);
+    setStartList(sortedList);
   };
 
-  const handleChooseCategory = (event, category) => {
-    let filteredList = [...movieList];
-    if (event.target.value === "Action") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes(category)
-      );
+  const handleChooseCategory = (event) => {
+    let category = event.target.value;
+    if (category === 'All') {
+      console.log(movieList);
+      setFilteredMovieList(movieList);
+      setStartList(movieList);
     }
+    console.log(movieList);
+    const filteredList = movieList.filter((movie) =>
+      movie.genres.includes(category)
+    );
 
-    if (event.target.value === "Adventure") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Adventure")
-      );
-    }
-
-    if (event.target.value === "Crime") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Crime")
-      );
-    }
-
-    if (event.target.value === "Drama") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Drama")
-      );
-    }
-
-    if (event.target.value === "Espionage") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Espionage")
-      );
-    }
-
-    if (event.target.value === "Fantasy") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Fantasy")
-      );
-    }
-
-    if (event.target.value === "Horror") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Horror")
-      );
-    }
-
-    if (event.target.value === "Music") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Music")
-      );
-    }
-
-    if (event.target.value === "Music") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Music")
-      );
-    }
-
-    if (event.target.value === "Mystery") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Mystery")
-      );
-    }
-
-    if (event.target.value === "Romance") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Romance")
-      );
-    }
-
-    if (event.target.value === "Science") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Science")
-      );
-    }
-
-    if (event.target.value === "Supernatural") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Supernatural")
-      );
-    }
-
-    if (event.target.value === "Thriller") {
-      filteredList = filteredList.filter((movie) =>
-        movie.genres.includes("Thriller")
-      );
-    }
-
-    console.log(filteredList);
-    setMovieList(filteredList);
-    setStartList(filteredList.slice(0, 15));
+    setFilteredMovieList(filteredList);
+    // setMovieList(filteredList);
+    setStartList(filteredList);
   };
 
   return (
@@ -134,6 +66,7 @@ const FilterBar = ({ movieList, setStartList, setMovieList }) => {
         <div className="category-select">
           <span>choose category:</span>
           <select onChange={handleChooseCategory}>
+            <option value="All">All</option>
             <option value="Action">Action</option>
             <option value="Adventure">Adventure</option>
             <option value="Crime">Crime</option>
